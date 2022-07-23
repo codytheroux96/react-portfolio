94fd2458a61ac2143cf19df4b84963ddbe84021f
cdt96 <cody.theroux3@gmail.com>
2022-07-23 14:07:12 -0400

    made an images folder and put a picture of me in it, made a navgiation in my index.js in nav folder

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
+function Nav (props) {
+	const { currentSection, setCurrentSection } = props;
 
+	return (
+		<nav>
+			<ul className='flex-row mobile-view'>
+				<li className={currentSection === 'About' ? 'mx-2 navActive' : 'mx-2'}>
+					<span onClick={() => setCurrentSection('About')}>About Me</span>
+				</li>
+				<li className={currentSection === 'Portfolio' ? 'mx-2 navActive' : 'mx-2'}>
+					<span onClick={() => setCurrentSection('Portfolio')}>Portfolio</span>
+				</li>
+				<li className={currentSection === 'Resume' ? 'mx-2 navActive' : 'mx-2'}>
+					<span onClick={() => setCurrentSection('Resume')}>Resume</span>
+				</li>
+				<li className={currentSection === 'Contact' ? 'mx-2 navActive' : 'mx-2'}>
+					<span onClick={() => setCurrentSection('Contact')}>Contact</span>
+				</li>
+			</ul>
+		</nav>
+	);
+}
 
 export default Nav;
