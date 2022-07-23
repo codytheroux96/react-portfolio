d31a48d3a6b61c8611704add2be9b32b486693b2
cdt96 <cody.theroux3@gmail.com>
2022-07-23 13:48:01 -0400

    made a render section function in my app.js and then a return section that will display the selected section

--- src/App.js
+++ src/App.js
@@ -6,10 +6,40 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Portfolio from './components/Portfolio';
-import Projects from './components/Projects';
+//import Projects from './components/Projects';
 import Resume from './components/Resume';
 
 function App() {
+  const [currentSection, setCurrentSection] = useState('About');
+
+  const renderSection = () => {
+    switch (currentSection) {
+      case 'About':
+        return <About />;
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Resume':
+        return <Resume />;
+      case 'Contact':
+        return <Contact />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+		<div>
+			<div className='Header'>
+				<Header currentSection={currentSection} setcurrentSection={setCurrentSection}></Header>
+			</div>
+			<div>
+				<main>{renderSection()}</main>
+			</div>
+			<div>
+				<Footer></Footer>
+			</div>
+		</div>
+	);
 
 }
 
