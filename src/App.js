cbc3c689988c253e028868fde8906f98b9795aa3
cdt96 <cody.theroux3@gmail.com>
2022-07-23 13:58:46 -0400

    imported and exported on all of my index.js's inside the components folder

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Nav from './components/Nav';
+//import Nav from './components/Nav';
 import Portfolio from './components/Portfolio';
 //import Projects from './components/Projects';
 import Resume from './components/Resume';
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Projects from '../Projects';
+
+
+
+
+export default Portfolio;
