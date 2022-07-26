57c1267fb8f4b6b9535d5f2913a8d5a67bb98abf
cdt96 <cody.theroux3@gmail.com>
2022-07-26 11:59:39 -0400

    imported images for my projects in portfolio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 //import Nav from './components/Nav';
 import Portfolio from './components/Portfolio';
-//import Projects from './components/Projects';
 import Resume from './components/Resume';
 
 
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
-//import Projects from '../Projects';
+import PantryPrepper from '../../assets/images/Pantry-Prepper.png';
+import TeamGenerator from '../../assets/images/Team-Generator.png';
+import BookNook from '../../assets/images/BookNook.png';
 
 
 function Portfolio() {
-  return (
+	return (
 		<section>
 			<div className="center" id="portfolio">
 				<h1 className="header">My Work</h1>
+			</div>
+
+			<div class="img-card">
+				<a href="">
+					<img src={BookNook} alt="Picture of BookNook" />
+					<p>BookNook</p>
+				</a>
+			</div>
+
+			<div class="img-card">
+				<a href="">
+					<img src={TeamGenerator} alt="Picture of Team Generator" />
+					<p>Team Generator</p>
+				</a>
+			</div>
+
+			<div class="img-card">
+				<a href="">
+					<img src={PantryPrepper} alt="Picture of Pantry Prepper" />
+					<p>Pantry Prepper</p>
+				</a>
 			</div>
 		</section>
 	);
