d20e60aaada3004c5dea04b237e8881ccdce43b9
cdt96 <cody.theroux3@gmail.com>
2022-07-26 12:13:08 -0400

    added a picture of me to about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Me from '../../assets/images/1656899586717.jpg'
 
 function About() {
 	return (
@@ -6,6 +7,7 @@ function About() {
 			<div className="center" id="about">
 				<h1 className="page-header">About Me</h1>
 			</div>
+			<img src = {Me} alt = "Picture of Me" />
 			<div>
 				<p>
 				Software developer who is passionate about tech and eager to always expand my knowledge and skills. I got into web development to make other people's lives easier and to work with a company that matches my values of how I believe people should be treated and is reflected in how they treat their clients/customers. I’m excited to use my talents of strong interpersonal communication paired with my ability to adapt quickly alongside my skills in web development to become a part of a fast-paced, quality-driven team to build better experiences for a growing world.
