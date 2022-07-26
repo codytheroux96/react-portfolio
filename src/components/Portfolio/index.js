b1901a0cc3eda21ec92f9cf8fa9731f4d7e6db58
cdt96 <cody.theroux3@gmail.com>
2022-07-26 14:56:31 -0400

    added descriptions for my projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -13,6 +13,7 @@ function Portfolio() {
 
 			<div class="img-card">
 				<h2>BookNook</h2>
+				<p>A database application that tracks and updates a digital library for users. Implemented with tools such as Nodejs, express, sequelize, dotenv, and axios.</p>
 					<img src={BookNook} alt="Picture of BookNook" />
 				<a href="https://dry-bastion-57106.herokuapp.com/login">
 					This is a link to the live site for this application
@@ -22,6 +23,7 @@ function Portfolio() {
 
 			<div class="img-card">
 				<h2>Team Generator</h2>
+				<p>This is a Node.js command line application that takes in information about employees on a software engineering team given by the user and generates an HTML webpage that displays summaries for each employee on dynamically generated cards.</p>
 					<img src={TeamGenerator} alt="Picture of Team Generator" />
 				<a href="https://github.com/codytheroux96/team-generator">
 				This is a link to the repo for this application
@@ -30,6 +32,7 @@ function Portfolio() {
 
 			<div class="img-card">
 				<h2>Pantry Prepper</h2>
+				<p>Page that leverages sever-side APIs to display ingredient recipes and prices</p>
 					<img src={PantryPrepper} alt="Picture of Pantry Prepper" />
 				<a href="https://kleylakb89.github.io/pantry-prepper/">
 					This is a link to the live site for this application
